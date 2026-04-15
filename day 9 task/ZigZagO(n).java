850153e62f7a17e147a73f29db9ccc02eb40852e
shyamigneshiyas <shyamigneshiyas2006@gmail.com>
2026-04-15 10:16:23 +0530

    Update ZigZagO(n).java

--- day 9 task/ZigZagO(n).java
+++ day 9 task/ZigZagO(n).java
@@ -1,23 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
         if (numRows == 1) return s;
-
-        StringBuilder res = new StringBuilder();
-        int cycle = 2 * numRows - 2;
-
-        for (int row = 0; row < numRows; row++) {
-            for (int j = row; j < s.length(); j += cycle) {
-                
-                
-                res.append(s.charAt(j));
-
-                
-                int diag = j + cycle - 2 * row;
-                if (row != 0 && row != numRows - 1 && diag < s.length()) {
-                    res.append(s.charAt(diag));
-                }
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for(int i =0; i < numRows; i++){
+            sb[i] = new StringBuilder();
+        }
+        boolean dir = false;
+        int curr = 0;
+        for(int i = 0; i<s.length();i++){
+            sb[curr].append(s.charAt(i));
+            if(curr == numRows -1 || curr == 0){
+                dir =! dir;
+            }
+            if(dir){
+                curr++;
+            }
+            else{
+                curr--;
             }
         }
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < sb.length; i++){
+            res.append(sb[i]);
+        }
         return res.toString();
+    
     }
 }
